7df8e98d250a9a91f5d5d0095d96799f0d0aa910
Javier Alejandro Ramírez Heredia <javieralejandroramirez69@gmail.com>
2024-08-15 19:00:00 -0600

    Commit para probar con flask :{

--- ui/fonts.py
+++
@@ -1,8 +0,0 @@
-# Definir las fuentes en un diccionario FONTS
-FONTS = {
-    "TITLE": ("Arial", 18, "bold"),     # Fuente para títulos
-    "SUBTITLE": ("Arial", 16, "italic"), # Fuente para subtítulos
-    "TEXT": ("Arial", 14),               # Fuente para el texto general
-    "BUTTON": ("Verdana", 14, "bold"),   # Fuente para botones
-    "TOAST": ("Arial", 12),               # Fuente para mensajes tipo toast
-}
